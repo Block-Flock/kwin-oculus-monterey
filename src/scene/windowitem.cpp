7946c590ad67d3a644a001734df0f6f1b1af02f0
Stanislav Aleksandrov <lightofmysoul@gmail.com>
2026-01-10 19:06:22 +0000

    scene: exclude VR windows from standard rendering
    Updates WindowItem to hide windows that have the VR property set.
    This prevents such windows from being rendered in the standard desktop scene.

--- src/scene/windowitem.cpp
+++ src/scene/windowitem.cpp
@@ -51,6 +51,7 @@ WindowItem::WindowItem(Window *window, Item *parent)
     connect(window, &Window::interactiveMoveResizeFinished, this, &WindowItem::updateVisibility);
     connect(window, &Window::outputChanged, this, &WindowItem::updateVisibility);
     connect(waylandServer(), &WaylandServer::lockStateChanged, this, &WindowItem::updateVisibility);
+    connect(window, &Window::vrChanged, this, &WindowItem::updateVisibility);
     connect(workspace(), &Workspace::currentActivityChanged, this, &WindowItem::updateVisibility);
     connect(workspace(), &Workspace::currentDesktopChanged, this, &WindowItem::updateVisibility);
     updateVisibility();
@@ -158,7 +159,7 @@ void WindowItem::deelevate()
 
 bool WindowItem::computeVisibility() const
 {
-    if (!m_window->readyForPainting()) {
+    if (!m_window->readyForPainting() || m_window->isVr()) {
         return false;
     }
     if (waylandServer()->isScreenLocked()) {
