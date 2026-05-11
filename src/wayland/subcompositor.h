Bake-Ware/kwin-vr

Add properties to surface and subcompositor needed for VR mode
SurfaceInterface: add subSurface and size properties
SubSurfaceInterface: add position property

src/wayland/subcompositor.h
@@ -33,7 +33,6 @@ class SubSurfaceInterfacePrivate;
 class KWIN_EXPORT SubCompositorInterface : public QObject
 {
     Q_OBJECT
-
 public:
     explicit SubCompositorInterface(Display *display, QObject *parent = nullptr);
     ~SubCompositorInterface() override;
@@ -54,7 +53,7 @@ private:
 class KWIN_EXPORT SubSurfaceInterface : public QObject
 {
     Q_OBJECT
-
+    Q_PROPERTY(QPoint position READ position NOTIFY positionChanged)
 public:
     ~SubSurfaceInterface() override;
 

Block-Flock/kwin-oculus-monterey

src/wayland/subcompositor.h
@@ -34,7 +34,6 @@ class SubSurfaceInterfacePrivate;
 class KWIN_EXPORT SubCompositorInterface : public QObject
 {
     Q_OBJECT
-
 public:
     explicit SubCompositorInterface(Display *display, QObject *parent = nullptr);
     ~SubCompositorInterface() override;
@@ -55,6 +54,7 @@ private:
 class KWIN_EXPORT SubSurfaceInterface : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QPointF position READ position NOTIFY positionChanged)
 
 public:
     ~SubSurfaceInterface() override;