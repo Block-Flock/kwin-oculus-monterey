Bake-Ware/kwin-vr

xdgshell: Allow customizing popup placement bounds
Add a settable callback to Workspace that allows plugins to override
how popup placement bounds are computed.

src/workspace.h
@@ -140,6 +140,10 @@ public:
     QRectF clientArea(clientAreaOption, const Window *window, const Output *output) const;
     QRectF clientArea(clientAreaOption, const Window *window, const QPointF &pos) const;
 
+    using PopupBoundsResolver = std::function<QRectF(Window *parent)>;
+    void setPopupBoundsResolver(PopupBoundsResolver resolver);
+    const PopupBoundsResolver &popupBoundsResolver() const;
+
     /**
      * Returns the geometry of this Workspace, i.e. the bounding rectangle of all outputs.
      */
@@ -750,6 +754,8 @@ private:
     QString m_activationToken;
     QString m_activationTokenAppId;
     uint32_t m_activationTokenSerial = 0;
+
+    PopupBoundsResolver m_popupBoundsResolver;
 
 private:
     friend bool performTransiencyCheck();

src/xdgshellwindow.cpp
@@ -1860,10 +1860,17 @@ void XdgPopupWindow::handleRepositionRequested(quint32 token)
     relayout();
 }
 
+std::function<QRectF(Window *parent)> XdgPopupWindow::defaultPopupBoundsResolver()
+{
+    return [](Window *parent) {
+        return workspace()->clientArea(parent->isFullScreen() ? FullScreenArea : PlacementArea, parent);
+    };
+}
+
 void XdgPopupWindow::updateRelativePlacement()
 {
     const QPointF parentPosition = transientFor()->nextFramePosToClientPos(transientFor()->pos());
-    const QRectF bounds = workspace()->clientArea(transientFor()->isFullScreen() ? FullScreenArea : PlacementArea, transientFor()).translated(-parentPosition);
+    const QRectF bounds = workspace()->popupBoundsResolver()(transientFor()).translated(-parentPosition);
     const XdgPositioner positioner = m_shellSurface->positioner();
 
     if (m_plasmaShellSurface && m_plasmaShellSurface->isPositionSet()) {

Block-Flock/kwin-oculus-monterey

src/workspace.h
@@ -155,6 +155,10 @@ public:
     RectF clientArea(clientAreaOption, const Window *window, const LogicalOutput *output) const;
     RectF clientArea(clientAreaOption, const Window *window, const QPointF &pos) const;
 
+    using PopupBoundsResolver = std::function<RectF(Window *parent)>;
+    void setPopupBoundsResolver(PopupBoundsResolver resolver);
+    const PopupBoundsResolver &popupBoundsResolver() const;
+
     /**
      * Returns the geometry of this Workspace, i.e. the bounding rectangle of all outputs.
      */
@@ -767,6 +771,8 @@ private:
     QList<QString> m_recentlyRemovedDpmsOffOutputs;
     QTimer m_dpmsTimer;
     std::shared_ptr<FileDescriptor> m_sleepInhibitor;
+
+    PopupBoundsResolver m_popupBoundsResolver;
 
 private:
     friend Workspace *workspace();

src/xdgshellwindow.cpp
@@ -1924,10 +1924,17 @@ void XdgPopupWindow::handleRepositionRequested(quint32 token)
     }
 }
 
+std::function<RectF(Window *parent)> XdgPopupWindow::defaultPopupBoundsResolver()
+{
+    return [](Window *parent) {
+        return workspace()->clientArea(parent->isFullScreen() ? FullScreenArea : PlacementArea, parent);
+    };
+}
+
 void XdgPopupWindow::updateRelativePlacement()
 {
     const QPointF parentPosition = transientFor()->nextFramePosToClientPos(transientFor()->pos());
-    const RectF bounds = workspace()->clientArea(transientFor()->isFullScreen() ? FullScreenArea : PlacementArea, transientFor()).translated(-parentPosition);
+    const RectF bounds = workspace()->popupBoundsResolver()(transientFor()).translated(-parentPosition);
     const XdgPositioner positioner = m_shellSurface->positioner();
 
     if (m_plasmaShellSurface && m_plasmaShellSurface->isPositionSet()) {